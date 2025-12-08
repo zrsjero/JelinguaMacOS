e232a92579f2d2ea3296a0c1d7ac1253aea73bb8
rzheltov <vtb4091205@region.vtb.ru>
2025-12-08 22:18:04 +0300

    Initial Commit

---
+++ jelingua-macos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jelingua-macos
+//
+//  Created by Roman Zheltov on 08.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jelingua-macos/jelingua_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  jelingua_macosApp.swift
+//  jelingua-macos
+//
+//  Created by Roman Zheltov on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct jelingua_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
